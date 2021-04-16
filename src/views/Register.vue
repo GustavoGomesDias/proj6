4e8846ce06075e0e1a9f0dffb1916ed042a2d840
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-16 01:49:50 -0300

    Aula 316 - Formulário simples

---
+++ src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Registro de Usuário</h2>
+        <hr>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <p>Nome</p>
+                <input type="text" placeholder="Nome do usuário" class="input is-half">
+                <p>E-mail</p>
+                <input type="email" placeholder="exemplo@exemplo.com" class="input">
+                <p>Senha</p>
+                <input type="password" class="input" placeholder="Senha">
+                <button class="button is-success">Cadastrar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
+
