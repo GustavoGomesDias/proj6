75f469d9831f23c051a7613634ce71c17e4b257e
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-19 22:41:06 -0300

    Aula 324 - Listando usuários

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,11 +1,62 @@
 <template>
     <h1>Painel Adm</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Cargo</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ processRole(user.role) }}</td>
+                <td>
+                    <button class="button is-success">Editar</button>|<button class="button is-danger">Deletar</button>
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
+
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
-    
+    // created é gerado assim que carrega a página
+    created(){
+        const req = {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        }
+        
+        axios.get("http://localhost:8686/user", req).then(res => {
+            console.log(res);
+            this.users = res.data;
+        }).catch(err => console.log(err))
+    },
+    data(){
+        return {
+            users: []
+        }
+    },
+    methods: {
+        processRole: function(value){
+            if(value == 0){
+                return "Usuário comum";
+            }else if(value == 1){
+                return "Admin";
+            }
+        }
+    }
 }
 
 </script>
