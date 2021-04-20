b3a29b6816b1218794293e8e757a04e091f8b0bf
GustavoGomesDias <treeelevengg@gmail.com>
2021-04-19 21:50:10 -0300

    Aula 323 - Terminando a validação do admin usando a role dele

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,30 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
-import Users from '../views/Users.vue'
+import Users from '../views/Users.vue';
+import axios from 'axios';
 
+function AdminAuth(to, from, next){
+  if(localStorage.getItem('token') != undefined){
+    const req = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token")
+      }
+    }
+
+    console.log(req);
+    
+    axios.post("http://localhost:8686/validate", {}, req).then(res => {
+      console.log(res);
+      next();
+    }).catch(err => {
+      console.log(err.response);
+      next("/login");
+    });
+  }else{
+    next("/login");
+  }
+}
 
 const routes = [
   {
@@ -25,13 +47,7 @@ const routes = [
     path: '/admin/users',
     name: 'Users',
     component: Users,
-    beforeEnter: (to, from, next) => {
-      if(localStorage.getItem('token') != undefined){
-        next();
-      }else{
-        next("/login");
-      }
-    }
+    beforeEnter: AdminAuth
   },
   {
     path: '/about',
